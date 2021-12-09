68e3b7c4572a56f308f41fa6132896c430b68eb9
tanakayayoi <march.yayoi31@gmail.com>
2021-12-09 19:59:30 +0900

    Shift only

--- ABC081B.rb
+++ ABC081B.rb
@@ -1,2 +1,17 @@
-N = gets.to_i
-A1 A2 .. AN = gets.chomp.split(' ').map(&:to_i)
+gets
+numbers = gets.chomp.split(' ').map(&:to_i)
+
+def shiftonly(numbers)
+  count = 0
+  counts = []
+  numbers.each do |num|
+    while num % 2 == 0
+      num /= 2
+      count += 1
+    end
+    counts = counts.push(count)
+    count = 0
+  end
+  p counts.min
+end
+shiftonly(numbers)
